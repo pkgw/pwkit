d26e044d1ef43909c9d8bfd4d683d8a6bed0ffb8
Peter Williams <peter@newton.cx>
2014-05-17 16:13:17 -0400

    setup.py: add some stuff to PyPI encourages

--- setup.py
+++ setup.py
@@ -26,4 +26,21 @@ setup (
     license = 'MIT',
     keywords = 'astronomy science',
     url = 'https://github.com/pkgw/pwkit/',
+
+    long_description = \
+    '''This is a collection of Peter Williams' miscellaneous Python tools. I'm
+    packaging them so that other people can install them off of PyPI and run
+    my code without having to go to too much work. That's the hope, at least.
+    ''',
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Astronomy',
+    ],
 )
