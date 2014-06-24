dcb57907b27ec656e0028340fddbec747d441caa
Peter Williams <peter@newton.cx>
2014-06-24 16:17:34 -0400

    pwkit/cli/imtool.py: import 'blink' subtool

--- pwkit/cli/imtool.py
+++ pwkit/cli/imtool.py
@@ -20,20 +20,92 @@ from .. import ndshow_gtk2 as ndshow # one day: flexible backend
 
 # The commands.
 
+def _blink_load (path, fft, maxnorm):
+    try:
+        img = astimage.open (path, 'r')
+    except Exception as e:
+        die ('can\'t open path “%s”: %s', path, e)
+
+    try:
+        img = img.simple ()
+    except Exception as e:
+        print ('blink: can\'t convert “%s” to simple 2D sky image; taking '
+               'first plane' % path, file=sys.stderr)
+        data = img.read (flip=True)[tuple (np.zeros (img.shape.size - 2))]
+        toworld = None
+    else:
+        data = img.read (flip=True)
+        toworld = img.toworld
+
+    if fft:
+        from numpy.fft import ifftshift, fft2, fftshift
+        data = np.abs (ifftshift (fft2 (fftshift (data.filled (0)))))
+        data = np.ma.MaskedArray (data)
+        toworld = None
+
+    if maxnorm:
+        data /= np.ma.max (data)
+
+    return data, toworld
+
+
+def cmd_blink (args):
+    fft = pop_option ('f', args)
+    maxnorm = pop_option ('m', args)
+
+    images = []
+    toworlds = []
+
+    for path in args:
+        image, toworld = _blink_load (path, fft, maxnorm)
+        images.append (image)
+        toworlds.append (toworld)
+
+    if not len (images):
+        return
+
+    shape = images[0].shape
+    for i, im in enumerate (images[1:]):
+        if im.shape != shape:
+            die ('shape of “%s” (%s) does not agree with that '
+                 'of “%s” (%s)',
+                 paths[i+1], '×'.join (map (str, im.shape)),
+                 paths[0], '×'.join (map (str, shape)))
+
+    # Merge masks. This is more complicated than you might think since you
+    # can't "or" nomask with itself.
+
+    jointmask = np.ma.nomask
+
+    for i in xrange (len (images)):
+        if jointmask is np.ma.nomask:
+            if images[i].mask is not np.ma.nomask:
+                jointmask = images[i].mask
+        else:
+            np.logical_or (jointmask, images[i].mask, jointmask)
+
+    for im in images:
+        im.mask = jointmask
+
+    ndshow.cycle (images, args, toworlds=toworlds, yflip=True)
+cmd_blink.argspec = '<images...>'
+cmd_blink.summary = 'Blink zero or more images.'
+
+
 def cmd_show (args):
     anyfailures = False
 
     for path in args:
         try:
             img = astimage.open (path, 'r')
-        except Exception, e:
+        except Exception as e:
             print ('pwshow: can\'t open path “%s”: %s' % (path, e), file=sys.stderr)
             anyfailures = True
             continue
 
         try:
             img = img.simple ()
-        except Exception, e:
+        except Exception as e:
             print ('pwshow: can\'t convert “%s” to simple 2D sky image; taking '
                    ' first plane' % path, file=sys.stderr)
             data = img.read (flip=True)[tuple (np.zeros (img.shape.size - 2))]
@@ -44,6 +116,8 @@ def cmd_show (args):
 
         ndshow.view (data, title=path + ' — Array Viewer',
                      toworld=toworld, yflip=True)
+
+    sys.exit (int (anyfailures))
 cmd_show.argspec = '<image> [images...]'
 cmd_show.summary = 'Show images interactively.'
 
@@ -99,8 +173,4 @@ def commandline (argv=None):
             print (func.moredocs)
         return
 
-    try:
-        func (args)
-    except UsageError as e:
-        print ('error:', e, '\n\nusage: imtool', cmdname, func.argspec,
-               file=sys.stderr)
+    func (args)
