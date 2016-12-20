aa798d3784493e650e2af008fd4606e8a5f5723c
Peter Williams <peter@newton.cx>
2016-12-20 11:15:06 -0500

    docs/source/conf.py: pngmath => imgmath
    I don't actually use any math but Sphinx was making warnings.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'sphinx.ext.pngmath',
+    'sphinx.ext.imgmath',
     'sphinx.ext.viewcode',
 ]
 
