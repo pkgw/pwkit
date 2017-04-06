85bd5a74b70f10b2dc135eaf8dbf7fd96cbb2629
Peter Williams <peter@newton.cx>
2017-04-06 08:44:13 -0400

    Bump to development version 0.8.5.99.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,8 +11,8 @@ extensions = [
 ]
 
 project = u'pwkit'
-version = '0.8.5' # also edit /setup.py, /pwkit/__init__.py!
-release = '0.8.5'
+version = '0.8.5.99' # also edit /setup.py, /pwkit/__init__.py!
+release = '0.8.5.99'
 
 copyright = u'2015-2017, Peter K. G. Williams and collaborators'
 author = u'Peter K. G. Williams and collaborators'
