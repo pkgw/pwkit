5f7d9d0699a37401f813421e9ae06212c70cc64a
Peter Williams <peter@newton.cx>
2017-11-02 14:41:28 -0400

    pwkit/environments/casa/tasks.py: hack no longer needed

--- pwkit/environments/casa/tasks.py
+++ pwkit/environments/casa/tasks.py
@@ -3605,8 +3605,7 @@ def tsysplot(cfg):
 
         for ifield in seenfields:
             antpols = apbyfield[ifield]
-            # this seems to be the only way to properly stringify a text-y numby.bytes 1D array:
-            kt = ''.join(chr(c) for c in fieldnames[ifield])
+            kt = fieldnames[ifield]
 
             for ispw, isoln in antpols.get((iant,ipol), []):
                 f = flags[ipol,:,isoln]
