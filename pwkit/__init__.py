aa2e170910314c6aa75379ede489430249f440a4
Peter Williams <peter@newton.cx>
2017-11-03 15:21:19 -0400

    pwkit/__init__.py: function call whitespace futzing

--- pwkit/__init__.py
+++ pwkit/__init__.py
@@ -7,7 +7,7 @@
 """
 from __future__ import absolute_import, division, print_function
 
-__all__ = 'Holder PKError binary_type reraise_context text_type unicode_to_str'.split ()
+__all__ = 'Holder PKError binary_type reraise_context text_type unicode_to_str'.split()
 
 __version__ = '0.8.12.99' # also edit ../setup.py, ../docs/source/conf.py!
 
@@ -19,12 +19,12 @@ import six
 from six import binary_type, text_type
 
 if six.PY2:
-    unicode_to_str = lambda s: s.__unicode__ ().encode ('utf8')
+    unicode_to_str = lambda s: s.__unicode__().encode('utf8')
 else:
-    unicode_to_str = lambda s: s.__unicode__ ()
+    unicode_to_str = lambda s: s.__unicode__()
 
 
-class PKError (Exception):
+class PKError(Exception):
     """A generic base class for exceptions.
 
     All custom exceptions raised by :mod:`pwkit` modules should be subclasses
@@ -33,7 +33,7 @@ class PKError (Exception):
     The constructor automatically applies old-fashioned ``printf``-like
     (``%``-based) string formatting if more than one argument is given::
 
-      PKError ('my format string says %r, %d', myobj, 12345)
+      PKError('my format string says %r, %d', myobj, 12345)
       # has text content equal to:
       'my format string says %r, %d' % (myobj, 12345)
 
@@ -41,22 +41,22 @@ class PKError (Exception):
     stringification without applying ``printf``-style formatting.
 
     """
-    def __init__ (self, fmt, *args):
-        if not len (args):
-            self.args = (text_type (fmt), )
+    def __init__(self, fmt, *args):
+        if not len(args):
+            self.args = (text_type(fmt), )
         else:
-            self.args = (text_type (fmt) % args, )
+            self.args = (text_type(fmt) % args, )
 
-    def __unicode__ (self):
+    def __unicode__(self):
         return self.args[0]
 
     __str__ = unicode_to_str
 
-    def __repr__ (self):
-        return 'PKError(' + repr (self.args[0]) + ')'
+    def __repr__(self):
+        return 'PKError(' + repr(self.args[0]) + ')'
 
 
-def reraise_context (fmt, *args):
+def reraise_context(fmt, *args):
     """Reraise an exception with its message modified to specify additional
     context.
 
@@ -82,11 +82,11 @@ def reraise_context (fmt, *args):
       filename = 'my-filename.txt'
 
       try:
-        f = Path (filename).open ('rt')
-        for line in f.readlines ():
+        f = Path(filename).open('rt')
+        for line in f.readlines():
           # do stuff ...
       except Exception as e:
-        reraise_context ('while reading "%r"', filename)
+        reraise_context('while reading "%r"', filename)
         # The exception is reraised and so control leaves this function.
 
     If an exception with text ``"bad value"`` were to be raised inside the
@@ -96,105 +96,105 @@ def reraise_context (fmt, *args):
     """
     import sys
 
-    if len (args):
+    if len(args):
         cstr = fmt % args
     else:
-        cstr = text_type (fmt)
+        cstr = text_type(fmt)
 
-    ex = sys.exc_info ()[1]
+    ex = sys.exc_info()[1]
 
-    if isinstance (ex, EnvironmentError):
+    if isinstance(ex, EnvironmentError):
         ex.strerror = '%s: %s' % (cstr, ex.strerror)
         ex.args = (ex.errno, ex.strerror)
     else:
-        if len (ex.args):
+        if len(ex.args):
             cstr = '%s: %s' % (cstr, ex.args[0])
         ex.args = (cstr, ) + ex.args[1:]
 
     raise
 
 
-class Holder (object):
+class Holder(object):
     """Create a new :class:`Holder`. Any keyword arguments will be assigned as
-    properties on the object itself, for instance, ``o = Holder (foo=1)``
+    properties on the object itself, for instance, ``o = Holder(foo=1)``
     yields an object such that ``o.foo`` is 1.
 
     The *__decorating* keyword is used to implement the :class:`Holder`
     decorator functionality, described below.
 
     """
-    def __init__ (self, __decorating=None, **kwargs):
+    def __init__(self, __decorating=None, **kwargs):
         import types
 
         if __decorating is None:
             values = kwargs
-        elif isinstance (__decorating, six.class_types):
+        elif isinstance(__decorating, six.class_types):
             # We're decorating a class definition. Transform the definition
             # into a Holder instance thusly:
-            values = dict (kv for kv in six.iteritems (__decorating.__dict__)
-                           if not kv[0].startswith ('__'))
+            values = dict(kv for kv in six.iteritems(__decorating.__dict__)
+                          if not kv[0].startswith('__'))
         else:
             # You could imagine allowing @Holder on a function and doing
             # something with its return value, but I can't think of a use that
             # would be more sensible than just creating and returning a Holder
             # directly.
-            raise ValueError ('unexpected use of Holder as a decorator (on %r)'
-                              % __decorating)
+            raise ValueError('unexpected use of Holder as a decorator (on %r)'
+                             % __decorating)
 
-        self.set (**values)
+        self.set(**values)
 
-    def __unicode__ (self):
+    def __unicode__(self):
         d = self.__dict__
-        s = sorted (six.iterkeys (d))
-        return u'{' + u', '.join (u'%s=%s' % (k, d[k]) for k in s) + u'}'
+        s = sorted(six.iterkeys(d))
+        return u'{' + u', '.join(u'%s=%s' % (k, d[k]) for k in s) + u'}'
 
     __str__ = unicode_to_str
 
-    def __repr__ (self):
+    def __repr__(self):
         d = self.__dict__
-        s = sorted (six.iterkeys (d))
+        s = sorted(six.iterkeys(d))
         return '%s(%s)' % (self.__class__.__name__,
-                            ', '.join ('%s=%r' % (k, d[k]) for k in s))
+                            ', '.join('%s=%r' % (k, d[k]) for k in s))
 
-    def __iter__ (self):
-        return six.iteritems (self.__dict__)
+    def __iter__(self):
+        return six.iteritems(self.__dict__)
 
-    def __contains__ (self, key):
+    def __contains__(self, key):
         return key in self.__dict__
 
-    def set (self, **kwargs):
+    def set(self, **kwargs):
         """For each keyword argument, sets an attribute on this :class:`Holder` to its
         value.
 
         Equivalent to::
 
-          for key, value in kwargs.iteritems ():
-            setattr (self, key, value)
+          for key, value in kwargs.iteritems():
+            setattr(self, key, value)
 
         Returns *self*.
 
         """
-        self.__dict__.update (kwargs)
+        self.__dict__.update(kwargs)
         return self
 
-    def get (self, name, defval=None):
+    def get(self, name, defval=None):
         """Get an attribute on this :class:`Holder`.
 
-        Equivalent to ``getattr (self, name, defval)``.
+        Equivalent to ``getattr(self, name, defval)``.
 
         """
-        return self.__dict__.get (name, defval)
+        return self.__dict__.get(name, defval)
 
-    def set_one (self, name, value):
+    def set_one(self, name, value):
         """Set a single attribute on this object.
 
-        Equivalent to ``setattr (self, name, value)``. Returns *self*.
+        Equivalent to ``setattr(self, name, value)``. Returns *self*.
 
         """
         self.__dict__[name] = value
         return self
 
-    def has (self, name):
+    def has(self, name):
         """Return whether the named attribute has been set on this object.
 
         This can more naturally be expressed by writing ``name in self``.
@@ -202,21 +202,21 @@ class Holder (object):
         """
         return name in self.__dict__
 
-    def copy (self):
+    def copy(self):
         """Return a shallow copy of this object.
 
         """
-        new = self.__class__ ()
-        new.__dict__ = dict (self.__dict__)
+        new = self.__class__()
+        new.__dict__ = dict(self.__dict__)
         return new
 
-    def to_dict (self):
+    def to_dict(self):
         """Return a copy of this object converted to a :class:`dict`.
 
         """
-        return self.__dict__.copy ()
+        return self.__dict__.copy()
 
-    def to_pretty (self, format='str'):
+    def to_pretty(self, format='str'):
         """Return a string with a prettified version of this object’s contents.
 
         The format is a multiline string where each line is of the form ``key
@@ -234,13 +234,13 @@ class Holder (object):
         elif format == 'repr':
             template = '%-*s = %r'
         else:
-            raise ValueError ('unrecognied value for "format": %r' % format)
+            raise ValueError('unrecognied value for "format": %r' % format)
 
         d = self.__dict__
         maxlen = 0
 
-        for k in six.iterkeys (d):
-            maxlen = max (maxlen, len (k))
+        for k in six.iterkeys(d):
+            maxlen = max(maxlen, len(k))
 
-        return '\n'.join (template % (maxlen, k, d[k])
-                          for k in sorted (six.iterkeys (d)))
+        return '\n'.join(template % (maxlen, k, d[k])
+                         for k in sorted(six.iterkeys(d)))
