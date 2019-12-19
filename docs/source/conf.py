0ffbaf2028cb30071b18d4f9d9c43b78fdebbc7f
Peter Williams <peter@newton.cx>
2019-12-19 13:42:53 -0500

    Release version 1.0.0.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,10 +11,10 @@ extensions = [
 ]
 
 project = u'pwkit'
-version = '0.8' # also edit /setup.py, /pwkit/__init__.py!
-release = '0.8.20.99'
+version = '1.0' # also edit /setup.py, /pwkit/__init__.py!
+release = '1.0.0'
 
-copyright = u'2015-2018, Peter K. G. Williams and collaborators'
+copyright = u'2015-2019, Peter K. G. Williams and collaborators'
 author = u'Peter K. G. Williams and collaborators'
 
 templates_path = ['_templates']
