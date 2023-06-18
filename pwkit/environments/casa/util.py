2903f5ae604afee88497905b4a11b04526ea74ea
Peter Williams <peter@newton.cx>
2023-06-18 16:45:30 -0400

    pwkit/environments/casa/util.py: reformat with black

--- pwkit/environments/casa/util.py
+++ pwkit/environments/casa/util.py
@@ -8,55 +8,115 @@
 """
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-__all__ = str('''INVERSE_C_MS INVERSE_C_MNS pol_names pol_to_miriad msselect_keys
-datadir logger forkandlog sanitize_unicode tools''').split()
+__all__ = str(
+    """INVERSE_C_MS INVERSE_C_MNS pol_names pol_to_miriad msselect_keys
+datadir logger forkandlog sanitize_unicode tools"""
+).split()
 
 import six
 from ... import text_type
 
 # Some constants that can be useful.
 
-INVERSE_C_MS  = 3.3356409519815204e-09 # inverse speed of light in m/s
-INVERSE_C_MNS = 3.3356409519815204 # inverse speed of light in m/ns
+INVERSE_C_MS = 3.3356409519815204e-09  # inverse speed of light in m/s
+INVERSE_C_MNS = 3.3356409519815204  # inverse speed of light in m/ns
 
 pol_names = {
-    0: '?',
-    1: 'I', 2: 'Q', 3: 'U', 4: 'V',
-    5: 'RR', 6: 'RL', 7: 'LR', 8: 'LL',
-    9: 'XX', 10: 'XY', 11: 'YX', 12: 'YY',
-    13: 'RX', 14: 'RY', 15: 'LX', 16: 'LY',
-    17: 'XR', 18: 'XL', 19: 'YR', 20: 'YL',
-    21: 'PP', 22: 'PQ', 23: 'QP', 24: 'QQ',
-    25: 'RCirc', 26: 'Lcirc', 27: 'Lin', 28: 'Ptot', 29: 'Plin',
-    30: 'PFtot', 31: 'PFlin', 32: 'Pang',
+    0: "?",
+    1: "I",
+    2: "Q",
+    3: "U",
+    4: "V",
+    5: "RR",
+    6: "RL",
+    7: "LR",
+    8: "LL",
+    9: "XX",
+    10: "XY",
+    11: "YX",
+    12: "YY",
+    13: "RX",
+    14: "RY",
+    15: "LX",
+    16: "LY",
+    17: "XR",
+    18: "XL",
+    19: "YR",
+    20: "YL",
+    21: "PP",
+    22: "PQ",
+    23: "QP",
+    24: "QQ",
+    25: "RCirc",
+    26: "Lcirc",
+    27: "Lin",
+    28: "Ptot",
+    29: "Plin",
+    30: "PFtot",
+    31: "PFlin",
+    32: "Pang",
 }
 
 pol_to_miriad = {
     # see mirtask.util for the MIRIAD magic numbers.
-    1: 1, 2: 2, 3: 3, 4: 4, # IQUV
-    5: -1, 6: -3, 7: -4, 8: -2, # R/L
-    9: -5, 10: -7, 11: -8, 12: -6, # X/Y
+    1: 1,
+    2: 2,
+    3: 3,
+    4: 4,  # IQUV
+    5: -1,
+    6: -3,
+    7: -4,
+    8: -2,  # R/L
+    9: -5,
+    10: -7,
+    11: -8,
+    12: -6,  # X/Y
     # rest are inexpressible
 }
 
 pol_is_intensity = {
     0: False,
-    1: True, 2: False, 3: False, 4: False, # IQUV
-    5: True, 6: False, 7: False, 8: True, # RR RL LR LL
-    9: True, 10: False, 11: False, 12: True, # XX XY YX YY
-    13: False, 14: False, 15: False, 16: False, # RX RY LX LY
-    17: False, 18: False, 19: False, 20: False, # XR XL YR YL
-    21: True, 22: False, 23: False, 24: True, # PP PQ QP QQ
-    25: False, 26: False, 27: False, 28: False, 29: False,
-    30: False, 31: False, 32: False,
+    1: True,
+    2: False,
+    3: False,
+    4: False,  # IQUV
+    5: True,
+    6: False,
+    7: False,
+    8: True,  # RR RL LR LL
+    9: True,
+    10: False,
+    11: False,
+    12: True,  # XX XY YX YY
+    13: False,
+    14: False,
+    15: False,
+    16: False,  # RX RY LX LY
+    17: False,
+    18: False,
+    19: False,
+    20: False,  # XR XL YR YL
+    21: True,
+    22: False,
+    23: False,
+    24: True,  # PP PQ QP QQ
+    25: False,
+    26: False,
+    27: False,
+    28: False,
+    29: False,
+    30: False,
+    31: False,
+    32: False,
 }
 
 # "polarization" is technically valid as an MS selection, but it pretty much
 # doesn't do what you'd want since records generally contain multiple pols.
 # ms.selectpolarization() should be used instead. Maybe ditto for spw?
 
-msselect_keys = frozenset('array baseline field observation '
-                          'scan scaninent spw taql time uvdist'.split())
+msselect_keys = frozenset(
+    "array baseline field observation " "scan scaninent spw taql time uvdist".split()
+)
 
 
 def sanitize_unicode(item):
@@ -90,13 +150,16 @@ def sanitize_unicode(item):
 
     """
     if isinstance(item, text_type):
-        return item.encode('utf8')
+        return item.encode("utf8")
     if isinstance(item, dict):
-        return dict((sanitize_unicode(k), sanitize_unicode(v)) for k, v in six.iteritems(item))
-    if isinstance(item,(list, tuple)):
+        return dict(
+            (sanitize_unicode(k), sanitize_unicode(v)) for k, v in six.iteritems(item)
+        )
+    if isinstance(item, (list, tuple)):
         return item.__class__(sanitize_unicode(x) for x in item)
 
     from ...io import Path
+
     if isinstance(item, Path):
         return str(item)
 
@@ -104,6 +167,7 @@ def sanitize_unicode(item):
 
 
 # Finding the data directory
+
 
 def datadir(*subdirs):
     """Get a path within the CASA data directory.
@@ -125,10 +189,11 @@ def datadir(*subdirs):
 
     """
     import os.path
+
     data = None
 
-    if 'CASAPATH' in os.environ:
-        data = os.path.join(os.environ['CASAPATH'].split()[0], 'data')
+    if "CASAPATH" in os.environ:
+        data = os.path.join(os.environ["CASAPATH"].split()[0], "data")
 
     if data is None:
         # The Conda CASA directory layout:
@@ -137,11 +202,13 @@ def datadir(*subdirs):
         except ImportError:
             pass
         else:
-            data = os.path.join(os.path.dirname(casadef.task_directory), 'data')
+            data = os.path.join(os.path.dirname(casadef.task_directory), "data")
             if not os.path.isdir(data):
                 # Sigh, hack for CASA 4.7 + Conda; should be straightened out:
                 dn = os.path.dirname
-                data = os.path.join(dn(dn(dn(casadef.task_directory))), 'lib', 'casa', 'data')
+                data = os.path.join(
+                    dn(dn(dn(casadef.task_directory))), "lib", "casa", "data"
+                )
                 if not os.path.isdir(data):
                     data = None
 
@@ -151,14 +218,14 @@ def datadir(*subdirs):
         prevp = None
         p = os.path.dirname(casac.__file__)
         while len(p) and p != prevp:
-            data = os.path.join(p, 'data')
+            data = os.path.join(p, "data")
             if os.path.isdir(data):
                 break
             prevp = p
             p = os.path.dirname(p)
 
     if not os.path.isdir(data):
-        raise RuntimeError('cannot identify CASA data directory')
+        raise RuntimeError("cannot identify CASA data directory")
 
     return os.path.join(data, *subdirs)
 
@@ -168,12 +235,14 @@ def datadir(*subdirs):
 # As soon as you create a logsink, it creates a file called casapy.log.
 # So we do some junk to not leave turds all around the filesystem.
 
+
 def _rmtree_error(func, path, excinfo):
     from ...cli import warn
-    warn('couldn\'t delete temporary file %s: %s (%s)', path, excinfo[0], func)
+
+    warn("couldn't delete temporary file %s: %s (%s)", path, excinfo[0], func)
 
 
-def logger(filter='WARN'):
+def logger(filter="WARN"):
     """Set up CASA to write log messages to standard output.
 
     filter
@@ -194,14 +263,14 @@ def logger(filter='WARN'):
     tempdir = None
 
     try:
-        tempdir = tempfile.mkdtemp(prefix='casautil')
+        tempdir = tempfile.mkdtemp(prefix="casautil")
 
         try:
             os.chdir(tempdir)
             sink = tools.logsink()
             sink.setlogfile(sanitize_unicode(os.devnull))
             try:
-                os.unlink('casapy.log')
+                os.unlink("casapy.log")
             except OSError as e:
                 if e.errno != 2:
                     raise
@@ -218,7 +287,7 @@ def logger(filter='WARN'):
     return sink
 
 
-def forkandlog(function, filter='INFO5', debug=False):
+def forkandlog(function, filter="INFO5", debug=False):
     """Fork a child process and read its CASA log output.
 
     function
@@ -278,12 +347,12 @@ def forkandlog(function, filter='INFO5', debug=False):
         os.close(readfd)
 
         if not debug:
-            f = open(os.devnull, 'w')
+            f = open(os.devnull, "w")
             os.dup2(f.fileno(), 1)
             os.dup2(f.fileno(), 2)
 
         sink = logger(filter=filter)
-        sink.setlogfile(b'/dev/fd/%d' % writefd)
+        sink.setlogfile(b"/dev/fd/%d" % writefd)
         function(sink)
         sys.exit(0)
 
@@ -300,13 +369,15 @@ def forkandlog(function, filter='INFO5', debug=False):
     if info[1]:
         # Because we're a generator, this is the only way for us to signal if
         # the process died. We could be rewritten as a context manager.
-        e = RuntimeError('logging child process PID %d exited '
-                         'with error code %d' % tuple(info))
+        e = RuntimeError(
+            "logging child process PID %d exited " "with error code %d" % tuple(info)
+        )
         e.pid, e.exitcode = info
         raise e
 
 
 # Tool factories.
+
 
 class _Tools(object):
     """This class is structured so that it supports useful tab-completion
@@ -314,12 +385,13 @@ class _Tools(object):
     underlying library provides them.
 
     """
-    _builtinNames = '''agentflagger atmosphere calanalysis calibrater calplot
+
+    _builtinNames = """agentflagger atmosphere calanalysis calibrater calplot
                      componentlist coordsys deconvolver fitter flagger
                      functional image imagepol imager logsink measures
                      msmetadata ms msplot mstransformer plotms regionmanager
                      simulator spectralline quanta table tableplot utils
-                     vlafiller vpmanager'''.split()
+                     vlafiller vpmanager""".split()
 
     def __getattribute__(self, n):
         """Returns factories, not instances."""
@@ -327,20 +399,21 @@ class _Tools(object):
         # we set the builtin names in the class __dict__ to enable tab-completion.
         import casac
 
-        if hasattr(casac, 'casac'): # casapy >= 4.0?
+        if hasattr(casac, "casac"):  # casapy >= 4.0?
             t = getattr(casac.casac, n, None)
             if t is None:
                 raise AttributeError('tool "%s" not present' % n)
             return t
         else:
             try:
-                return casac.homefinder.find_home_by_name(n + 'Home').create
+                return casac.homefinder.find_home_by_name(n + "Home").create
             except Exception:
                 # raised exception is class 'homefinder.error'; it appears unavailable
                 # on the Python layer
                 raise AttributeError('tool "%s" not present' % n)
 
+
 for n in _Tools._builtinNames:
-    setattr(_Tools, n, None) # ease autocompletion
+    setattr(_Tools, n, None)  # ease autocompletion
 
 tools = _Tools()
