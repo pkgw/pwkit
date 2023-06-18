c034c2f65c2ca473db374995e0ad58a378c048a5
Peter Williams <peter@newton.cx>
2023-06-18 15:20:19 -0400

    pwkit/colormaps.py: reformat with "black"

--- pwkit/colormaps.py
+++ pwkit/colormaps.py
@@ -76,20 +76,22 @@ I made up the pkgw map myself (who'd have guessed?).
 """
 from __future__ import absolute_import, division, print_function
 
-__all__ = 'base_factory_names factory_map'.split()
+__all__ = "base_factory_names factory_map".split()
 
 
 import numpy as np, six
 
-base_factory_names = ('moreland_bluered cubehelix_dagreen cubehelix_blue '
-                      'pkgw black_to_white black_to_red '
-                      'black_to_green black_to_blue '
-                      'white_to_black white_to_red '
-                      'white_to_green white_to_blue').split()
+base_factory_names = (
+    "moreland_bluered cubehelix_dagreen cubehelix_blue "
+    "pkgw black_to_white black_to_red "
+    "black_to_green black_to_blue "
+    "white_to_black white_to_red "
+    "white_to_green white_to_blue"
+).split()
 
 R, G, B = range(3)
 X, Y, Z = range(3)
-L, A, B = range(3) # fortunately this B and RGB's B agree...
+L, A, B = range(3)  # fortunately this B and RGB's B agree...
 M, S, H = range(3)
 
 DEFAULT_SAMPLE_POINTS = 512
@@ -97,22 +99,23 @@ DEFAULT_SAMPLE_POINTS = 512
 
 # I don't quite understand where this value comes from, given the various
 # Wikipedia values for D65, but this works.
-CIELAB_D65 = np.asarray([0.9505, 1., 1.0890])
+CIELAB_D65 = np.asarray([0.9505, 1.0, 1.0890])
 
 # from Moreland:
-_linsrgb_to_xyz = np.asarray([[0.4124, 0.2126, 0.0193],
-                              [0.3576, 0.7152, 0.1192],
-                              [0.1805, 0.0722, 0.9505]])
+_linsrgb_to_xyz = np.asarray(
+    [[0.4124, 0.2126, 0.0193], [0.3576, 0.7152, 0.1192], [0.1805, 0.0722, 0.9505]]
+)
 
 # from Wikipedia, SRGB:
-_xyz_to_linsrgb = np.asarray([[3.2406, -0.9689, 0.0557],
-                              [-1.5372, 1.8758, -0.2040],
-                              [-0.4986, 0.0415, 1.0570]])
+_xyz_to_linsrgb = np.asarray(
+    [[3.2406, -0.9689, 0.0557], [-1.5372, 1.8758, -0.2040], [-0.4986, 0.0415, 1.0570]]
+)
 
 
 # Interpolation utilities.
 
-def approx_colormap(samples, transform='none', fitfactor=1.):
+
+def approx_colormap(samples, transform="none", fitfactor=1.0):
     """Given a colormap sampled at various values, compute splines that
     interpolate in R, G, and B (separately) for fast evaluation of the
     colormap for arbitrary float values. We have primitive support for some
@@ -135,19 +138,19 @@ def approx_colormap(samples, transform='none', fitfactor=1.):
     import scipy.interpolate as SI
 
     values = samples[0]
-    if transform == 'none':
+    if transform == "none":
         pass
-    elif transform == 'reverse':
-        samples = samples[:,::-1]
-    elif transform == 'sqrt':
+    elif transform == "reverse":
+        samples = samples[:, ::-1]
+    elif transform == "sqrt":
         values = np.sqrt(values)
     else:
-        raise ValueError('unknown transformation: ' + str(transform))
+        raise ValueError("unknown transformation: " + str(transform))
 
     nsamp = samples.shape[1]
-    rspline = SI.splrep(values, samples[R+1], s=fitfactor/nsamp)
-    gspline = SI.splrep(values, samples[G+1], s=fitfactor/nsamp)
-    bspline = SI.splrep(values, samples[B+1], s=fitfactor/nsamp)
+    rspline = SI.splrep(values, samples[R + 1], s=fitfactor / nsamp)
+    gspline = SI.splrep(values, samples[G + 1], s=fitfactor / nsamp)
+    bspline = SI.splrep(values, samples[B + 1], s=fitfactor / nsamp)
 
     def colormap(values):
         values = np.asarray(values)
@@ -156,9 +159,9 @@ def approx_colormap(samples, transform='none', fitfactor=1.):
         flatvalues = values.flatten()
         flatmapped = mapped.reshape(flatvalues.shape + (3,))
 
-        flatmapped[:,R] = SI.splev(flatvalues, rspline)
-        flatmapped[:,G] = SI.splev(flatvalues, gspline)
-        flatmapped[:,B] = SI.splev(flatvalues, bspline)
+        flatmapped[:, R] = SI.splev(flatvalues, rspline)
+        flatmapped[:, G] = SI.splev(flatvalues, gspline)
+        flatmapped[:, B] = SI.splev(flatvalues, bspline)
 
         return mapped
 
@@ -167,6 +170,7 @@ def approx_colormap(samples, transform='none', fitfactor=1.):
 
 # Colorspace utilities based on the Moreland paper.
 
+
 def srgb_to_linsrgb(srgb):
     """Convert sRGB values to physically linear ones. The transformation is
     uniform in RGB, so *srgb* can be of any shape.
@@ -174,7 +178,7 @@ def srgb_to_linsrgb(srgb):
     *srgb* values should range between 0 and 1, inclusively.
 
     """
-    gamma = ((srgb + 0.055) / 1.055)**2.4
+    gamma = ((srgb + 0.055) / 1.055) ** 2.4
     scale = srgb / 12.92
     return np.where(srgb > 0.04045, gamma, scale)
 
@@ -187,7 +191,7 @@ def linsrgb_to_srgb(linsrgb):
 
     """
     # From Wikipedia, but easy analogue to the above.
-    gamma = 1.055 * linsrgb**(1./2.4) - 0.055
+    gamma = 1.055 * linsrgb ** (1.0 / 2.4) - 0.055
     scale = linsrgb * 12.92
     return np.where(linsrgb > 0.0031308, gamma, scale)
 
@@ -227,13 +231,13 @@ def xyz_to_cielab(xyz, refwhite):
     """
     norm = xyz / refwhite
     pow = norm**0.333333333333333
-    scale = 7.787037 * norm + 16./116
+    scale = 7.787037 * norm + 16.0 / 116
     mapped = np.where(norm > 0.008856, pow, scale)
 
     cielab = np.empty_like(xyz)
-    cielab[...,L] = 116 * mapped[...,Y] - 16
-    cielab[...,A] = 500 * (mapped[...,X] - mapped[...,Y])
-    cielab[...,B] = 200 * (mapped[...,Y] - mapped[...,Z])
+    cielab[..., L] = 116 * mapped[..., Y] - 16
+    cielab[..., A] = 500 * (mapped[..., X] - mapped[..., Y])
+    cielab[..., B] = 200 * (mapped[..., Y] - mapped[..., Z])
 
     return cielab
 
@@ -247,16 +251,17 @@ def cielab_to_xyz(cielab, refwhite):
     Return value has same shape as *cielab*
 
     """
+
     def func(t):
         pow = t**3
         scale = 0.128419 * t - 0.0177129
         return np.where(t > 0.206897, pow, scale)
 
     xyz = np.empty_like(cielab)
-    lscale = 1./116 * (cielab[...,L] + 16)
-    xyz[...,X] = func(lscale + 0.002 * cielab[...,A])
-    xyz[...,Y] = func(lscale)
-    xyz[...,Z] = func(lscale - 0.005 * cielab[...,B])
+    lscale = 1.0 / 116 * (cielab[..., L] + 16)
+    xyz[..., X] = func(lscale + 0.002 * cielab[..., A])
+    xyz[..., Y] = func(lscale)
+    xyz[..., Z] = func(lscale - 0.005 * cielab[..., B])
     xyz *= refwhite
     return xyz
 
@@ -270,9 +275,9 @@ def cielab_to_msh(cielab):
 
     """
     msh = np.empty_like(cielab)
-    msh[...,M] = np.sqrt((cielab**2).sum(axis=-1))
-    msh[...,S] = np.arccos(cielab[...,L] / msh[...,M])
-    msh[...,H] = np.arctan2(cielab[...,B], cielab[...,A])
+    msh[..., M] = np.sqrt((cielab**2).sum(axis=-1))
+    msh[..., S] = np.arccos(cielab[..., L] / msh[..., M])
+    msh[..., H] = np.arctan2(cielab[..., B], cielab[..., A])
     return msh
 
 
@@ -285,9 +290,9 @@ def msh_to_cielab(msh):
 
     """
     cielab = np.empty_like(msh)
-    cielab[...,L] = msh[...,M] * np.cos(msh[...,S])
-    cielab[...,A] = msh[...,M] * np.sin(msh[...,S]) * np.cos(msh[...,H])
-    cielab[...,B] = msh[...,M] * np.sin(msh[...,S]) * np.sin(msh[...,H])
+    cielab[..., L] = msh[..., M] * np.cos(msh[..., S])
+    cielab[..., A] = msh[..., M] * np.sin(msh[..., S]) * np.cos(msh[..., H])
+    cielab[..., B] = msh[..., M] * np.sin(msh[..., S]) * np.sin(msh[..., H])
     return cielab
 
 
@@ -317,6 +322,7 @@ def msh_to_srgb(msh, refwhite):
 
 # The Moreland divergent colormap generation algorithm.
 
+
 def moreland_adjusthue(msh, m_unsat):
     """Moreland's AdjustHue procedure to adjust the hue value of an Msh color
     based on ... some criterion.
@@ -327,17 +333,18 @@ def moreland_adjusthue(msh, m_unsat):
 
     """
     if msh[M] >= m_unsat:
-        return msh[H] # "Best we can do"
+        return msh[H]  # "Best we can do"
 
-    hspin = (msh[S] * np.sqrt(m_unsat**2 - msh[M]**2) / (msh[M] * np.sin(msh[S])))
+    hspin = msh[S] * np.sqrt(m_unsat**2 - msh[M] ** 2) / (msh[M] * np.sin(msh[S]))
 
-    if msh[H] > -np.pi / 3: # "Spin away from purple"
+    if msh[H] > -np.pi / 3:  # "Spin away from purple"
         return msh[H] + hspin
     return msh[H] - hspin
 
 
-def moreland_interpolate_sampled(srgb1, srgb2, refwhite=CIELAB_D65,
-                                 nsamples=DEFAULT_SAMPLE_POINTS):
+def moreland_interpolate_sampled(
+    srgb1, srgb2, refwhite=CIELAB_D65, nsamples=DEFAULT_SAMPLE_POINTS
+):
     """Adapted from Moreland's InterpolateColor. This uses the full
     transformations to compute a color mapping at a set of sampled points."""
 
@@ -353,7 +360,7 @@ def moreland_interpolate_sampled(srgb1, srgb2, refwhite=CIELAB_D65,
     x = np.linspace(0, 1, nsamples).reshape((nsamples, 1))
     x = np.repeat(x, 3, 1)
 
-    if msh1[S] <= 0.05 or msh2[S] <= 0.05 or raddiff < np.pi/3:
+    if msh1[S] <= 0.05 or msh2[S] <= 0.05 or raddiff < np.pi / 3:
         # Colors are too close together to comfortably put white in between.
         # Our interpolation won't have a control point, and won't actually be
         # divergent.
@@ -366,7 +373,7 @@ def moreland_interpolate_sampled(srgb1, srgb2, refwhite=CIELAB_D65,
         samples = np.empty((4, nsamples))
 
         msh = (1 - x) * msh1 + x * msh2
-        samples[0] = x[:,0]
+        samples[0] = x[:, 0]
         samples[1:4] = msh_to_srgb(msh, refwhite).T
     else:
         # Colors are not too close together -- we can add a white control
@@ -388,26 +395,28 @@ def moreland_interpolate_sampled(srgb1, srgb2, refwhite=CIELAB_D65,
         elif msh3[S] < 0.05 and msh2b[S] > 0.05:
             msh3[H] = moreland_adjusthue(msh2b, msh3[M])
 
-        samples = np.empty((4, 2*nsamples-1))
+        samples = np.empty((4, 2 * nsamples - 1))
 
         msh = (1 - x) * msh1 + x * msh2a
-        samples[0,:nsamples] = 0.5 * x[:,0]
-        samples[1:4,:nsamples] = msh_to_srgb(msh, refwhite).T
+        samples[0, :nsamples] = 0.5 * x[:, 0]
+        samples[1:4, :nsamples] = msh_to_srgb(msh, refwhite).T
 
         msh = (1 - x) * msh2b + x * msh3
-        samples[0,nsamples-1:] = 0.5 * x[:,0] + 0.5
-        samples[1:4,nsamples-1:] = msh_to_srgb(msh, refwhite).T
+        samples[0, nsamples - 1 :] = 0.5 * x[:, 0] + 0.5
+        samples[1:4, nsamples - 1 :] = msh_to_srgb(msh, refwhite).T
 
     return samples
 
 
-def moreland_bluered(transform='none'):
-    samples = moreland_interpolate_sampled([0.2305, 0.2969, 0.7500],
-                                           [0.7031, 0.0156, 0.1484])
+def moreland_bluered(transform="none"):
+    samples = moreland_interpolate_sampled(
+        [0.2305, 0.2969, 0.7500], [0.7031, 0.0156, 0.1484]
+    )
     return approx_colormap(samples, transform=transform)
 
 
 # D. A. Green's "cube helix" colormap
+
 
 def cubehelix_create(start, rotations, hue, gamma):
     def colormap(values):
@@ -417,33 +426,39 @@ def cubehelix_create(start, rotations, hue, gamma):
         flatvalues = values.flatten()
         flatmapped = mapped.reshape(flatvalues.shape + (3,))
 
-        gv = flatvalues ** gamma
+        gv = flatvalues**gamma
         a = 0.5 * hue * gv * (1 - gv)
         phi = 2 * np.pi * (0.3333333 * start + rotations * flatvalues)
         c = np.cos(phi)
         s = np.sin(phi)
 
-        flatmapped[:,R] = gv + a * (-0.14861 * c + 1.78277 * s)
-        flatmapped[:,G] = gv + a * (-0.29227 * c - 0.90649 * s)
-        flatmapped[:,B] = gv + a * 1.97294 * c
+        flatmapped[:, R] = gv + a * (-0.14861 * c + 1.78277 * s)
+        flatmapped[:, G] = gv + a * (-0.29227 * c - 0.90649 * s)
+        flatmapped[:, B] = gv + a * 1.97294 * c
 
         return mapped
+
     return colormap
 
 
 def cubehelix_sample(start, rotations, hue, gamma, nsamples=DEFAULT_SAMPLE_POINTS):
-    samples = np.empty((4, nsamples,))
+    samples = np.empty(
+        (
+            4,
+            nsamples,
+        )
+    )
     samples[0] = np.linspace(0, 1, nsamples)
     samples[1:] = cubehelix_create(start, rotations, hue, gamma)(samples[0]).T
     return samples
 
 
-def cubehelix_dagreen(transform='none'):
+def cubehelix_dagreen(transform="none"):
     samples = cubehelix_sample(0.5, -1.5, 1.0, 1)
     return approx_colormap(samples, transform=transform)
 
 
-def cubehelix_blue(transform='none'):
+def cubehelix_blue(transform="none"):
     samples = cubehelix_sample(0.5, -0.6, 1.2, 1)
     return approx_colormap(samples, transform=transform)
 
@@ -451,14 +466,15 @@ def cubehelix_blue(transform='none'):
 # Something quick I came up with based on the Moreland work, scaling from
 # black to a bright-ish red.
 
-def pkgw(transform='none', nsamples=DEFAULT_SAMPLE_POINTS):
+
+def pkgw(transform="none", nsamples=DEFAULT_SAMPLE_POINTS):
     samples = np.empty((4, nsamples))
     samples[0] = np.linspace(0, 1, nsamples)
 
     msh = np.empty((nsamples, 3))
-    msh[:,M] = 1. + 85 * samples[0]
-    msh[:,S] = 0.3 * samples[0] + 0.7
-    msh[:,H] = 2.9 * samples[0] - 2.1
+    msh[:, M] = 1.0 + 85 * samples[0]
+    msh[:, S] = 0.3 * samples[0] + 0.7
+    msh[:, H] = 2.9 * samples[0] - 2.1
 
     samples[1:4] = msh_to_srgb(msh, CIELAB_D65).T
 
@@ -467,55 +483,62 @@ def pkgw(transform='none', nsamples=DEFAULT_SAMPLE_POINTS):
 
 # Simple maps linear in RGB
 
-def rgblinear_create(factor_r, factor_g, factor_b,
-                     zero_r, zero_g, zero_b,
-                     transform='none'):
-    if transform == 'none':
+
+def rgblinear_create(
+    factor_r, factor_g, factor_b, zero_r, zero_g, zero_b, transform="none"
+):
+    if transform == "none":
         valmap = lambda x: x
-    elif transform == 'reverse':
+    elif transform == "reverse":
         valmap = lambda x: 1 - x
-    elif transform == 'sqrt':
+    elif transform == "sqrt":
         valmap = np.sqrt
     else:
-        raise ValueError('unknown transformation: ' + str(transform))
+        raise ValueError("unknown transformation: " + str(transform))
 
     def colormap(values):
         values = valmap(np.asarray(values))
         mapped = np.empty(values.shape + (3,))
         flatvalues = values.flatten()
         flatmapped = mapped.reshape(flatvalues.shape + (3,))
-        flatmapped[:,R] = flatvalues * factor_r + zero_r
-        flatmapped[:,G] = flatvalues * factor_g + zero_g
-        flatmapped[:,B] = flatvalues * factor_b + zero_b
+        flatmapped[:, R] = flatvalues * factor_r + zero_r
+        flatmapped[:, G] = flatvalues * factor_g + zero_g
+        flatmapped[:, B] = flatvalues * factor_b + zero_b
         return mapped
 
     return colormap
 
 
-def black_to_white(transform='none'):
+def black_to_white(transform="none"):
     return rgblinear_create(1, 1, 1, 0, 0, 0, transform)
 
-def black_to_red(transform='none'):
+
+def black_to_red(transform="none"):
     return rgblinear_create(1, 0, 0, 0, 0, 0, transform)
 
-def black_to_green(transform='none'):
+
+def black_to_green(transform="none"):
     return rgblinear_create(0, 1, 0, 0, 0, 0, transform)
 
-def black_to_blue(transform='none'):
+
+def black_to_blue(transform="none"):
     return rgblinear_create(0, 0, 1, 0, 0, 0, transform)
 
-def white_to_black(transform='none'):
+
+def white_to_black(transform="none"):
     return rgblinear_create(-1, -1, -1, 1, 1, 1, transform)
 
-def white_to_red(transform='none'):
+
+def white_to_red(transform="none"):
     return rgblinear_create(0, -1, -1, 1, 1, 1, transform)
 
-def white_to_green(transform='none'):
+
+def white_to_green(transform="none"):
     return rgblinear_create(-1, 0, -1, 1, 1, 1, transform)
 
-def white_to_blue(transform='none'):
-    return rgblinear_create(-1, -1, 0, 1, 1, 1, transform)
 
+def white_to_blue(transform="none"):
+    return rgblinear_create(-1, -1, 0, 1, 1, 1, transform)
 
 
 # Useful for introspection
@@ -526,30 +549,35 @@ def white_to_blue(transform='none'):
 
 factory_map = dict((n, globals()[n]) for n in base_factory_names)
 
+
 def _make_transformed(factory, transform):
     # We have to create these helper functions in a separate function because
     # otherwise some all of the new dict entries end up referencing the
     # finally-created subfunction.
     def newfactory():
         return factory(transform=transform)
+
     return newfactory
 
+
 def _fill_transforms():
-    for transform in('reverse', 'sqrt'):
+    for transform in ("reverse", "sqrt"):
         for n in base_factory_names:
             factory = globals()[n]
-            factory_map[n + '_' + transform] = \
-                _make_transformed(factory, transform)
+            factory_map[n + "_" + transform] = _make_transformed(factory, transform)
+
 
 _fill_transforms()
 
 
 # Infrastructure for quickly rendering color maps.
 
+
 def showdemo(factoryname, **kwargs):
     import gi
-    gi.require_version('Gdk', '3.0')
-    gi.require_version('Gtk', '3.0')
+
+    gi.require_version("Gdk", "3.0")
+    gi.require_version("Gtk", "3.0")
     from gi.repository import GLib, GObject, Gdk, Gtk
     import cairo
 
@@ -564,9 +592,9 @@ def showdemo(factoryname, **kwargs):
     mapped = colormap(array)
     argb = np.empty((H, W), dtype=np.uint32)
     argb.fill(0xFF000000)
-    argb |= (mapped[:,:,0] * 0xFF).astype(np.uint32) << 16
-    argb |= (mapped[:,:,1] * 0xFF).astype(np.uint32) << 8
-    argb |= (mapped[:,:,2] * 0xFF).astype(np.uint32)
+    argb |= (mapped[:, :, 0] * 0xFF).astype(np.uint32) << 16
+    argb |= (mapped[:, :, 1] * 0xFF).astype(np.uint32) << 8
+    argb |= (mapped[:, :, 2] * 0xFF).astype(np.uint32)
 
     surf = cairo.ImageSurface.create_for_data(argb, cairo.FORMAT_ARGB32, W, H, W * 4)
 
@@ -579,26 +607,27 @@ def showdemo(factoryname, **kwargs):
         return True
 
     da = Gtk.DrawingArea()
-    da.connect('draw', draw)
+    da.connect("draw", draw)
 
     win = Gtk.Window(type=Gtk.WindowType.TOPLEVEL)
-    win.set_title('Colormap Demo - ' + factoryname)
+    win.set_title("Colormap Demo - " + factoryname)
     win.set_default_size(W, H)
-    win.connect('destroy', Gtk.main_quit)
+    win.connect("destroy", Gtk.main_quit)
     win.add(da)
     win.show_all()
     Gtk.main()
 
 
 def printmaps():
-    print('Available color maps:')
+    print("Available color maps:")
 
     for m in sorted(six.iterkeys(factory_map)):
-        print('\t' + m)
+        print("\t" + m)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
+
     if len(sys.argv) < 2:
         printmaps()
     else:
