efd56eea293b3494fe4fb54c6b6f3cebd71f297d
Peter Williams <peter@newton.cx>
2023-06-18 14:47:41 -0400

    docs/source/conf.py: reformat with "black"

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,72 +3,72 @@
 import sphinx_rtd_theme
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.imgmath',
-    'sphinx.ext.viewcode',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.imgmath",
+    "sphinx.ext.viewcode",
 ]
 
-project = u'pwkit'
-version = 'master' # also edit /setup.py, /pwkit/__init__.py!
-release = '1.1.0.dev0'
+project = "pwkit"
+version = "master"  # also edit /setup.py, /pwkit/__init__.py!
+release = "1.1.0.dev0"
 
-copyright = u'2015-2019, Peter K. G. Williams and collaborators'
-author = u'Peter K. G. Williams and collaborators'
+copyright = "2015-2019, Peter K. G. Williams and collaborators"
+author = "Peter K. G. Williams and collaborators"
 
-templates_path = ['_templates']
-source_suffix = '.rst'
-master_doc = 'index'
+templates_path = ["_templates"]
+source_suffix = ".rst"
+master_doc = "index"
 language = None
 exclude_patterns = []
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
-#add_function_parentheses = True
+# add_function_parentheses = True
 
 # If true, the current module name will be prepended to all description
 # unit titles (such as .. function::).
-#add_module_names = True
+# add_module_names = True
 
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 todo_include_todos = False
 
 
 # Intersphinx
 
 intersphinx_mapping = {
-    'https://docs.python.org/': None,
+    "https://docs.python.org/": None,
 }
 
 
 # HTML output settings
 
-html_theme = 'sphinx_rtd_theme'
-#html_theme_options = {}
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path ()]
-html_static_path = ['_static']
-htmlhelp_basename = 'pwkitdoc'
+html_theme = "sphinx_rtd_theme"
+# html_theme_options = {}
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_static_path = ["_static"]
+htmlhelp_basename = "pwkitdoc"
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+# html_title = None
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+# html_short_title = None
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+# html_logo = None
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+# html_favicon = None
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
-#html_extra_path = []
+# html_extra_path = []
 
 
 # Tomfoolery to fake modules that readthedocs.org doesn't know. We need to do
@@ -78,26 +78,31 @@ htmlhelp_basename = 'pwkitdoc'
 import sys
 from mock import Mock as MagicMock
 
-class Mock (MagicMock):
-    @classmethod
-    def __getattr__ (cls, name):
-        if name in ('ArithmeticModel', 'XSAdditiveModel'):
-            return dict
-        if name == 'CompositeModel':
-            return Mock
-        return Mock ()
 
-sys.modules.update ((m, Mock ()) for m in [
-    'cairo',
-    'gi',
-    'gi.repository',
-    'glib',
-    'gtk',
-    'sherpa',
-    'sherpa.astro',
-    'sherpa.astro.ui',
-    'sherpa.astro.xspec',
-    'sherpa.astro.xspec._xspec',
-    'sherpa.models',
-    'sherpa.models.parameter',
-])
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ("ArithmeticModel", "XSAdditiveModel"):
+            return dict
+        if name == "CompositeModel":
+            return Mock
+        return Mock()
+
+
+sys.modules.update(
+    (m, Mock())
+    for m in [
+        "cairo",
+        "gi",
+        "gi.repository",
+        "glib",
+        "gtk",
+        "sherpa",
+        "sherpa.astro",
+        "sherpa.astro.ui",
+        "sherpa.astro.xspec",
+        "sherpa.astro.xspec._xspec",
+        "sherpa.models",
+        "sherpa.models.parameter",
+    ]
+)
