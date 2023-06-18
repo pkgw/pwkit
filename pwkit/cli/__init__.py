ebdf91ff2aefb9badf524a5b06ffcd2a9a74410b
Peter Williams <peter@newton.cx>
2023-06-18 16:28:02 -0400

    pwkit/cli/__init__.py: reformat with black

--- pwkit/cli/__init__.py
+++ pwkit/cli/__init__.py
@@ -28,15 +28,17 @@ multitool - Framework for command-line programs with sub-commands.
 """
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-__all__ = str ('''check_usage die fork_detached_process pop_option print_tracebacks
+__all__ = str(
+    """check_usage die fork_detached_process pop_option print_tracebacks
                   propagate_sigint show_usage unicode_stdio warn
-                  wrong_usage''').split ()
+                  wrong_usage"""
+).split()
 
 import codecs, os, signal, six, sys, traceback
 from .. import text_type
 
 
-def unicode_stdio ():
+def unicode_stdio():
     """Make sure that the standard I/O streams accept Unicode.
 
     In Python 2, the standard I/O streams accept bytes, not Unicode
@@ -63,15 +65,15 @@ def unicode_stdio ():
     if six.PY3:
         return
 
-    enc = sys.stdin.encoding or 'utf-8'
-    sys.stdin = codecs.getreader (enc) (sys.stdin)
+    enc = sys.stdin.encoding or "utf-8"
+    sys.stdin = codecs.getreader(enc)(sys.stdin)
     enc = sys.stdout.encoding or enc
-    sys.stdout = codecs.getwriter (enc) (sys.stdout)
+    sys.stdout = codecs.getwriter(enc)(sys.stdout)
     enc = sys.stderr.encoding or enc
-    sys.stderr = codecs.getwriter (enc) (sys.stderr)
+    sys.stderr = codecs.getwriter(enc)(sys.stderr)
 
 
-class _InterruptSignalPropagator (object):
+class _InterruptSignalPropagator(object):
     """Ensure that calling shells know when we die from SIGINT.
 
     Imagine that a shell script is running a long-running subprogram and the
@@ -106,9 +108,10 @@ class _InterruptSignalPropagator (object):
     `pwkit.cli.propagate_sigint.inner_excepthook`.
 
     """
+
     inner_excepthook = None
 
-    def __call__ (self):
+    def __call__(self):
         """Set sys.excepthook to our special version.
 
         It chains to the previous excepthook, of course. This value can be
@@ -119,42 +122,49 @@ class _InterruptSignalPropagator (object):
             self.inner_excepthook = sys.excepthook
             sys.excepthook = self.excepthook
 
-    def excepthook (self, etype, evalue, etb):
+    def excepthook(self, etype, evalue, etb):
         """Handle an uncaught exception. We always forward the exception on to
         whatever `sys.excepthook` was present upon setup. However, if the
         exception is a KeyboardInterrupt, we additionally kill ourselves with
         an uncaught SIGINT, so that invoking programs know what happened.
 
         """
-        self.inner_excepthook (etype, evalue, etb)
+        self.inner_excepthook(etype, evalue, etb)
 
-        if issubclass (etype, KeyboardInterrupt):
+        if issubclass(etype, KeyboardInterrupt):
             # Don't try this at home, kids. On some systems os.kill (0, ...)
             # signals our entire progress group, which is not what we want,
             # so we use os.getpid ().
-            signal.signal (signal.SIGINT, signal.SIG_DFL)
-            os.kill (os.getpid (), signal.SIGINT)
-
-propagate_sigint = _InterruptSignalPropagator ()
+            signal.signal(signal.SIGINT, signal.SIG_DFL)
+            os.kill(os.getpid(), signal.SIGINT)
 
 
-def _print_backtrace_signal_handler (signum, frame):
+propagate_sigint = _InterruptSignalPropagator()
+
+
+def _print_backtrace_signal_handler(signum, frame):
     try:
         import traceback
-        print ('*** Printing traceback due to receipt of signal #%d' % signum,
-               file=sys.stderr)
-        for fn, line, func, text in traceback.extract_stack (frame):
-            print ('***   %s (%s:%d): %s' % (fn, func, line, text or '??'),
-                   file=sys.stderr)
-        print ('*** End of traceback (innermost call is last)',
-               file=sys.stderr)
+
+        print(
+            "*** Printing traceback due to receipt of signal #%d" % signum,
+            file=sys.stderr,
+        )
+        for fn, line, func, text in traceback.extract_stack(frame):
+            print(
+                "***   %s (%s:%d): %s" % (fn, func, line, text or "??"), file=sys.stderr
+            )
+        print("*** End of traceback (innermost call is last)", file=sys.stderr)
         assert False
     except Exception as e:
-        print ('*** Failed to print traceback on receipt of signal #%d: %s (%s)'
-               % (signum, e, e.__class__.__name__), file=sys.stderr)
+        print(
+            "*** Failed to print traceback on receipt of signal #%d: %s (%s)"
+            % (signum, e, e.__class__.__name__),
+            file=sys.stderr,
+        )
 
 
-def backtrace_on_usr1 ():
+def backtrace_on_usr1():
     """Install a signal handler such that this program prints a Python traceback
     upon receipt of SIGUSR1. This could be useful for checking that
     long-running programs are behaving properly, or for discovering where an
@@ -168,13 +178,14 @@ def backtrace_on_usr1 ():
 
     """
     import signal
+
     try:
-        signal.signal (signal.SIGUSR1, _print_backtrace_signal_handler)
+        signal.signal(signal.SIGUSR1, _print_backtrace_signal_handler)
     except Exception as e:
-        warn ('failed to set up Python backtraces on SIGUSR1: %s', e)
+        warn("failed to set up Python backtraces on SIGUSR1: %s", e)
 
 
-def die (fmt, *args):
+def die(fmt, *args):
     """Raise a :exc:`SystemExit` exception with a formatted error message.
 
     :arg str fmt: a format string
@@ -191,21 +202,21 @@ def die (fmt, *args):
           die ('require exactly 3 dimensions, not %d', ndim)
 
     """
-    if not len (args):
-        raise SystemExit ('error: ' + text_type (fmt))
-    raise SystemExit ('error: ' + (fmt % args))
+    if not len(args):
+        raise SystemExit("error: " + text_type(fmt))
+    raise SystemExit("error: " + (fmt % args))
 
 
-def warn (fmt, *args):
-    if not len (args):
-        s = text_type (fmt)
+def warn(fmt, *args):
+    if not len(args):
+        s = text_type(fmt)
     else:
         s = fmt % args
 
-    print ('warning:', s, file=sys.stderr)
+    print("warning:", s, file=sys.stderr)
 
 
-class print_tracebacks (object):
+class print_tracebacks(object):
     """Context manager that catches exceptions and prints their tracebacks without
     reraising them. Intended for robust programs that want to continue
     execution even if something bad happens; this provides the infrastructure
@@ -224,24 +235,25 @@ class print_tracebacks (object):
     fact, occur.
 
     """
-    header = 'Swallowed exception:'
 
-    def __init__ (self, types=(Exception,), header=None, file=None):
+    header = "Swallowed exception:"
+
+    def __init__(self, types=(Exception,), header=None, file=None):
         self.types = types
         self.file = file
 
         if header is not None:
             self.header = header
 
-    def __enter__ (self):
+    def __enter__(self):
         self.etype = self.evalue = self.etb = None
         return self
 
-    def __exit__ (self, etype, evalue, etb):
+    def __exit__(self, etype, evalue, etb):
         if etype is None:
-            return False # all good, woohoo
+            return False  # all good, woohoo
 
-        if not isinstance (evalue, self.types):
+        if not isinstance(evalue, self.types):
             # Exception happened but not something of the kind we expect. Reraise.
             return False
 
@@ -251,14 +263,15 @@ class print_tracebacks (object):
         self.etb = etb
 
         if self.header is not None:
-            print (self.header, file=self.file or sys.stderr)
+            print(self.header, file=self.file or sys.stderr)
 
         from traceback import print_exception
-        print_exception (etype, evalue, etb, file=self.file)
-        return True # swallow this exception
+
+        print_exception(etype, evalue, etb, file=self.file)
+        return True  # swallow this exception
 
 
-def fork_detached_process ():
+def fork_detached_process():
     """Fork this process, creating a subprocess detached from the current context.
 
     Returns a :class:`pwkit.Holder` instance with information about what
@@ -309,45 +322,47 @@ def fork_detached_process ():
     """
     import os, struct
     from .. import Holder
-    payload = struct.Struct ('L')
 
-    info = Holder ()
-    readfd, writefd = os.pipe ()
+    payload = struct.Struct("L")
 
-    pid1 = os.fork ()
+    info = Holder()
+    readfd, writefd = os.pipe()
+
+    pid1 = os.fork()
     if pid1 > 0:
-        info.whoami = 'original'
-        info.pipe = os.fdopen (readfd, 'rb')
-        os.close (writefd)
+        info.whoami = "original"
+        info.pipe = os.fdopen(readfd, "rb")
+        os.close(writefd)
 
-        retcode = os.waitpid (pid1, 0)[1]
+        retcode = os.waitpid(pid1, 0)[1]
         if retcode:
-            raise Exception ('child process exited with error code %d' % retcode)
+            raise Exception("child process exited with error code %d" % retcode)
 
-        (info.forkedpid,) = payload.unpack (info.pipe.read (payload.size))
+        (info.forkedpid,) = payload.unpack(info.pipe.read(payload.size))
     else:
         # We're the intermediate child process. Start new session and process
         # groups, detaching us from TTY signals and whatnot.
-        os.setsid ()
+        os.setsid()
 
-        pid2 = os.fork ()
+        pid2 = os.fork()
         if pid2 > 0:
             # We're the intermediate process; we're all done
-            os._exit (0)
+            os._exit(0)
 
         # If we get here, we're the detached child process.
-        info.whoami = 'forked'
-        info.pipe = os.fdopen (writefd, 'wb')
-        os.close (readfd)
-        info.forkedpid = os.getpid ()
-        info.pipe.write (payload.pack (info.forkedpid))
+        info.whoami = "forked"
+        info.pipe = os.fdopen(writefd, "wb")
+        os.close(readfd)
+        info.forkedpid = os.getpid()
+        info.pipe.write(payload.pack(info.forkedpid))
 
     return info
 
 
 # Simple-minded argument handling -- see also kwargv.
 
-def pop_option (ident, argv=None):
+
+def pop_option(ident, argv=None):
     """A lame routine for grabbing command-line arguments. Returns a boolean
     indicating whether the option was present. If it was, it's removed from
     the argument string. Because of the lame behavior, options can't be
@@ -360,19 +375,19 @@ def pop_option (ident, argv=None):
     if argv is None:
         from sys import argv
 
-    if len (ident) == 1:
-        ident = '-' + ident
+    if len(ident) == 1:
+        ident = "-" + ident
     else:
-        ident = '--' + ident
+        ident = "--" + ident
 
     found = ident in argv
     if found:
-        argv.remove (ident)
+        argv.remove(ident)
 
     return found
 
 
-def show_usage (docstring, short, stream, exitcode):
+def show_usage(docstring, short, stream, exitcode):
     """Print program usage information and exit.
 
     :arg str docstring: the program help text
@@ -392,25 +407,27 @@ def show_usage (docstring, short, stream, exitcode):
         from sys import stdout as stream
 
     if not short:
-        print ('Usage:', docstring.strip (), file=stream)
+        print("Usage:", docstring.strip(), file=stream)
     else:
         intext = False
-        for l in docstring.splitlines ():
+        for l in docstring.splitlines():
             if intext:
-                if not len (l):
+                if not len(l):
                     break
-                print (l, file=stream)
-            elif len (l):
+                print(l, file=stream)
+            elif len(l):
                 intext = True
-                print ('Usage:', l, file=stream)
+                print("Usage:", l, file=stream)
 
-        print ('\nRun with a sole argument --help for more detailed '
-               'usage information.', file=stream)
+        print(
+            "\nRun with a sole argument --help for more detailed " "usage information.",
+            file=stream,
+        )
 
-    raise SystemExit (exitcode)
+    raise SystemExit(exitcode)
 
 
-def check_usage (docstring, argv=None, usageifnoargs=False):
+def check_usage(docstring, argv=None, usageifnoargs=False):
     """Check if the program has been run with a --help argument; if so,
     print usage information and exit.
 
@@ -441,13 +458,13 @@ def check_usage (docstring, argv=None, usageifnoargs=False):
     if argv is None:
         from sys import argv
 
-    if len (argv) == 1 and usageifnoargs:
-        show_usage (docstring, (usageifnoargs != 'long'), None, 0)
-    if len (argv) == 2 and argv[1] in ('-h', '--help'):
-        show_usage (docstring, False, None, 0)
+    if len(argv) == 1 and usageifnoargs:
+        show_usage(docstring, (usageifnoargs != "long"), None, 0)
+    if len(argv) == 2 and argv[1] in ("-h", "--help"):
+        show_usage(docstring, False, None, 0)
 
 
-def wrong_usage (docstring, *rest):
+def wrong_usage(docstring, *rest):
     """Print a message indicating invalid command-line arguments and exit with an
     error code.
 
@@ -489,12 +506,12 @@ def wrong_usage (docstring, *rest):
     """
     intext = False
 
-    if len (rest) == 0:
-        detail = 'invalid command-line arguments'
-    elif len (rest) == 1:
+    if len(rest) == 0:
+        detail = "invalid command-line arguments"
+    elif len(rest) == 1:
         detail = rest[0]
     else:
-        detail = rest[0] % tuple (rest[1:])
+        detail = rest[0] % tuple(rest[1:])
 
-    print ('error:', detail, '\n', file=sys.stderr) # extra NL
-    show_usage (docstring, True, sys.stderr, 1)
+    print("error:", detail, "\n", file=sys.stderr)  # extra NL
+    show_usage(docstring, True, sys.stderr, 1)
