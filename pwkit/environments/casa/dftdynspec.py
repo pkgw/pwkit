bb0f8827a7de4b80030a5bbb2b38a9a93fce5c0c
Peter Williams <peter@newton.cx>
2023-06-18 16:37:24 -0400

    pwkit/environments/casa/dftdynspec.py: reformat with black

--- pwkit/environments/casa/dftdynspec.py
+++ pwkit/environments/casa/dftdynspec.py
@@ -12,7 +12,7 @@ Sets. CASA doesn't have a task that does this.
 """
 from __future__ import absolute_import, division, print_function
 
-__all__ = 'Config Loader dftdynspec dftdynspec_cli'.split()
+__all__ = "Config Loader dftdynspec dftdynspec_cli".split()
 
 import io, os.path, sys
 import numpy as np
@@ -20,16 +20,16 @@ import six
 from six.moves import range
 
 from ... import binary_type, text_type
+
 # Note: zany spacing so that Sphinx can parse the file correctly.
-from . ..astutil import *
-from . ..cli import check_usage, die
-from . ..io import get_stdout_bytes
-from . ..kwargv import ParseKeywords, Custom
+from ...astutil import *
+from ...cli import check_usage, die
+from ...io import get_stdout_bytes
+from ...kwargv import ParseKeywords, Custom
 from . import util
 from .util import sanitize_unicode as b
 
-dftdynspec_doc = \
-"""
+dftdynspec_doc = """
 casatask dftdynspec vis=<MS> [keywords...]
 
 Extract a dynamic spectrum from the visibilities in a measurement set. See
@@ -81,17 +81,18 @@ used to load the data into a Python program.
 
 """
 
+
 class Config(ParseKeywords):
     vis = Custom(str, required=True)
-    datacol = 'data'
+    datacol = "data"
     believeweights = False
 
-    @Custom(str, uiname='out')
+    @Custom(str, uiname="out")
     def outstream(val):
         if val is None:
             return get_stdout_bytes()
         try:
-            return open(val, 'wb')
+            return open(val, "wb")
         except Exception as e:
             die('cannot open path "%s" for writing', val)
 
@@ -112,7 +113,7 @@ class Config(ParseKeywords):
     baseline = str
     field = str
     observation = str
-    polarization = 'RR,LL'
+    polarization = "RR,LL"
     scan = str
     scanintent = str
     spw = str
@@ -120,7 +121,7 @@ class Config(ParseKeywords):
     time = str
     uvdist = str
 
-    loglevel = 'warn'
+    loglevel = "warn"
 
 
 def dftdynspec(cfg):
@@ -144,40 +145,43 @@ def dftdynspec(cfg):
     # selectinit() is broken, but the invocation here is good because it
     # affects the results from ms.range() and friends.
 
-    if ':' in (cfg.spw or ''):
-        warn('it looks like you are attempting to select channels within one or more spws')
-        warn('this is NOT IMPLEMENTED; I will process the whole spw instead')
+    if ":" in (cfg.spw or ""):
+        warn(
+            "it looks like you are attempting to select channels within one or more spws"
+        )
+        warn("this is NOT IMPLEMENTED; I will process the whole spw instead")
 
     ms.open(b(cfg.vis))
     totrows = ms.nrow()
-    ms_sels = dict((n, cfg.get(n)) for n in util.msselect_keys
-                   if cfg.get(n) is not None)
+    ms_sels = dict(
+        (n, cfg.get(n)) for n in util.msselect_keys if cfg.get(n) is not None
+    )
     ms.msselect(b(ms_sels))
 
-    rangeinfo = ms.range(b'data_desc_id field_id'.split())
-    ddids = rangeinfo['data_desc_id']
-    fields = rangeinfo['field_id']
-    colnames = [cfg.datacol] + 'flag weight time axis_info'.split()
-    rephase = (cfg.rephase is not None)
+    rangeinfo = ms.range(b"data_desc_id field_id".split())
+    ddids = rangeinfo["data_desc_id"]
+    fields = rangeinfo["field_id"]
+    colnames = [cfg.datacol] + "flag weight time axis_info".split()
+    rephase = cfg.rephase is not None
 
     if fields.size != 1:
         # I feel comfortable making this a fatal error, even if we're
         # not rephasing.
-        die('selected data should contain precisely one field; got %d', fields.size)
+        die("selected data should contain precisely one field; got %d", fields.size)
 
-    tb.open(b(os.path.join(cfg.vis, 'DATA_DESCRIPTION')))
-    ddspws = tb.getcol(b'SPECTRAL_WINDOW_ID')
+    tb.open(b(os.path.join(cfg.vis, "DATA_DESCRIPTION")))
+    ddspws = tb.getcol(b"SPECTRAL_WINDOW_ID")
     tb.close()
 
     # Get frequencies and precompute merged, sorted frequency array
     # FIXME: below we get 'freqs' on the fly; should honor that.
     # But then mapping and data storage get super inefficient.
 
-    tb.open(b(os.path.join(cfg.vis, 'SPECTRAL_WINDOW')))
+    tb.open(b(os.path.join(cfg.vis, "SPECTRAL_WINDOW")))
     nspw = tb.nrows()
     spwfreqs = []
     for i in range(nspw):
-        spwfreqs.append(tb.getcell(b'CHAN_FREQ', i) * 1e-9) # -> GHz
+        spwfreqs.append(tb.getcell(b"CHAN_FREQ", i) * 1e-9)  # -> GHz
     tb.close()
 
     allfreqs = set()
@@ -192,14 +196,17 @@ def dftdynspec(cfg):
 
     if rephase:
         fieldid = fields[0]
-        tb.open(b(os.path.join(cfg.vis, 'FIELD')))
-        phdirinfo = tb.getcell(b'PHASE_DIR', fieldid)
+        tb.open(b(os.path.join(cfg.vis, "FIELD")))
+        phdirinfo = tb.getcell(b"PHASE_DIR", fieldid)
         tb.close()
 
         if phdirinfo.shape[1] != 1:
-            die('trying to rephase but target field (#%d) has a '
-                'time-variable phase center, which I can\'t handle', fieldid)
-        ra0, dec0 = phdirinfo[:,0] # in radians.
+            die(
+                "trying to rephase but target field (#%d) has a "
+                "time-variable phase center, which I can't handle",
+                fieldid,
+            )
+        ra0, dec0 = phdirinfo[:, 0]  # in radians.
 
         # based on intflib/pwflux.py, which was copied from
         # hex/hex-lib-calcgainerr:
@@ -209,9 +216,9 @@ def dftdynspec(cfg):
         l = np.sin(dra) * np.cos(dec)
         m = np.sin(dec) * np.cos(dec0) - np.cos(dra) * np.cos(dec) * np.sin(dec0)
         n = np.sin(dec) * np.sin(dec0) + np.cos(dra) * np.cos(dec) * np.cos(dec0)
-        n -= 1 # makes the work below easier
+        n -= 1  # makes the work below easier
         lmn = np.asarray([l, m, n])
-        colnames.append('uvw')
+        colnames.append("uvw")
 
     tbins = {}
     colnames = b(colnames)
@@ -220,12 +227,12 @@ def dftdynspec(cfg):
         # Starting in CASA 4.6, selectinit(ddid) stopped actually filtering
         # your data to match the specified DDID! What garbage. Work around
         # with our own filtering.
-        ms_sels['taql'] = 'DATA_DESC_ID == %d' % ddid
+        ms_sels["taql"] = "DATA_DESC_ID == %d" % ddid
         ms.msselect(b(ms_sels))
 
         ms.selectinit(ddid)
         if cfg.polarization is not None:
-            ms.selectpolarization(b(cfg.polarization.split(',')))
+            ms.selectpolarization(b(cfg.polarization.split(",")))
         ms.iterinit(maxrows=4096)
         ms.iterorigin()
 
@@ -238,34 +245,34 @@ def dftdynspec(cfg):
                 # With appropriate spw/DDID selection, `freqs` has shape
                 # (nchan, 1). Convert to m^-1 so we can multiply against UVW
                 # directly.
-                freqs = cols['axis_info']['freq_axis']['chan_freq']
-                assert freqs.shape[1] == 1, 'internal inconsistency, chan_freq??'
-                freqs = freqs[:,0] * util.INVERSE_C_MS
+                freqs = cols["axis_info"]["freq_axis"]["chan_freq"]
+                assert freqs.shape[1] == 1, "internal inconsistency, chan_freq??"
+                freqs = freqs[:, 0] * util.INVERSE_C_MS
 
-            for i in range(cols['time'].size): # all records
-                time = cols['time'][i]
+            for i in range(cols["time"].size):  # all records
+                time = cols["time"][i]
                 # get out of UTC as fast as we can! For some reason
                 # giving 'unit=s' below doesn't do what one might hope it would.
                 # CASA can convert to a variety of timescales; TAI is probably
                 # the safest conversion in terms of being helpful while remaining
                 # close to the fundamental data, but TT is possible and should
                 # be perfectly precise for standard applications.
-                mq = me.epoch(b'utc', b({'value': time / 86400., 'unit': 'd'}))
-                mjdtt = me.measure(b(mq), b'tt')['m0']['value']
+                mq = me.epoch(b"utc", b({"value": time / 86400.0, "unit": "d"}))
+                mjdtt = me.measure(b(mq), b"tt")["m0"]["value"]
 
                 tdata = tbins.get(mjdtt)
                 if tdata is None:
                     tdata = tbins[mjdtt] = np.zeros((nfreq, 7))
 
                 if rephase:
-                    uvw = cols['uvw'][:,i]
-                    ph = np.exp((0-2j) * np.pi * np.dot(lmn, uvw) * freqs)
+                    uvw = cols["uvw"][:, i]
+                    ph = np.exp((0 - 2j) * np.pi * np.dot(lmn, uvw) * freqs)
 
-                for j in range(cols['flag'].shape[0]): # all polns
+                for j in range(cols["flag"].shape[0]):  # all polns
                     # We just average together all polarizations right now!
                     # (Not actively, but passively by just iterating over them.)
-                    data = cols[cfg.datacol][j,:,i]
-                    flags = cols['flag'][j,:,i]
+                    data = cols[cfg.datacol][j, :, i]
+                    flags = cols["flag"][j, :, i]
 
                     # XXXXX casacore is currently broken and returns the raw
                     # weights from the dataset rather than applying the
@@ -275,27 +282,27 @@ def dftdynspec(cfg):
 
                     w = np.where(~flags)[0]
                     if not w.size:
-                        continue # all flagged
+                        continue  # all flagged
 
                     if rephase:
                         data *= ph
 
                     m = freqmaps[spwid][w]
                     d = data[w]
-                    wt = cols['weight'][j,i]
+                    wt = cols["weight"][j, i]
 
-                    tdata[m,0] += wt * d.real
-                    tdata[m,1] += wt * d.imag
-                    tdata[m,2] += wt * d.real**2
-                    tdata[m,3] += wt * d.imag**2
-                    tdata[m,4] += wt
-                    tdata[m,5] += wt**2
-                    tdata[m,6] += 1
+                    tdata[m, 0] += wt * d.real
+                    tdata[m, 1] += wt * d.imag
+                    tdata[m, 2] += wt * d.real**2
+                    tdata[m, 3] += wt * d.imag**2
+                    tdata[m, 4] += wt
+                    tdata[m, 5] += wt**2
+                    tdata[m, 6] += 1
 
             if not ms.iternext():
                 break
 
-        ms.reset() # reset selection filter so we can get next DDID
+        ms.reset()  # reset selection filter so we can get next DDID
 
     ms.close()
 
@@ -309,28 +316,28 @@ def dftdynspec(cfg):
 
         wr, wi, wr2, wi2, wt, wt2, n = tbins[mjd].T
         w = np.where(n > 0)[0]
-        if w.size < 3: # not enough data for meaningful statistics
+        if w.size < 3:  # not enough data for meaningful statistics
             continue
 
         r = wr[w] / wt[w]
         i = wi[w] / wt[w]
 
         if cfg.believeweights:
-            ru = wt[w]**-0.5
-            iu = wt[w]**-0.5
+            ru = wt[w] ** -0.5
+            iu = wt[w] ** -0.5
         else:
             r2 = wr2[w] / wt[w]
             i2 = wi2[w] / wt[w]
-            rv = r2 - r**2 # variance among real/imag msmts
+            rv = r2 - r**2  # variance among real/imag msmts
             iv = i2 - i**2
-            ru = np.sqrt(rv * wt2[w]) / wt[w] # uncert in mean real/img values
+            ru = np.sqrt(rv * wt2[w]) / wt[w]  # uncert in mean real/img values
             iu = np.sqrt(iv * wt2[w]) / wt[w]
 
-        data[0,tid,w] = r
-        data[1,tid,w] = ru
-        data[2,tid,w] = i
-        data[3,tid,w] = iu
-        data[4,tid,w] = n[w]
+        data[0, tid, w] = r
+        data[1, tid, w] = ru
+        data[2, tid, w] = i
+        data[3, tid, w] = iu
+        data[4, tid, w] = n[w]
 
     np.save(cfg.outstream, smjd)
     np.save(cfg.outstream, allfreqs)
@@ -353,6 +360,7 @@ class Loader(object):
       The path of the file to read.
 
     """
+
     mjds = None
     "A 1D sorted array of the MJDs of the data samples."
 
@@ -384,19 +392,18 @@ class Loader(object):
     average. Shape is `(mjds.size, freqs.size)`.
 
     """
+
     def __init__(self, path):
-        with io.open(path, 'rb') as f:
+        with io.open(path, "rb") as f:
             self.mjds = np.load(f)
             self.freqs = np.load(f)
             cube = np.load(f)
             self.reals, self.u_reals, self.imags, self.u_imags, self.counts = cube
 
-
     @property
     def n_mjds(self):
         "The size of the MJD axis of the data arrays; an integer."
         return self.mjds.size
-
 
     @property
     def n_freqs(self):
